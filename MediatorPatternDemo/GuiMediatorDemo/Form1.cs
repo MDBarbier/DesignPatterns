838ba380ccde946a3a95e31a8f6b32929426073b
matt barbier <mdbarbier@protonmail.com>
2021-02-10 20:19:33 +0000

    Added further mediator demos

---
+++ MediatorPatternDemo/GuiMediatorDemo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuiMediatorDemo
+{
+    public partial class Form1 : Form
+    {
+        private MarkerMediator mediator = new MarkerMediator();
+        private Button addButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.addButton = new Button();
+            this.addButton.Click += OnAddClick;
+            this.addButton.Text = "Add marker";
+            this.addButton.Dock = DockStyle.Bottom;
+            this.Controls.Add(this.addButton);
+
+        }
+
+        private void OnAddClick(object sender, EventArgs e)
+        {
+            //Via the mediator, create a new marker
+            var m = this.mediator.CreateMarker();
+
+            //Add the new marker to the screen
+            this.Controls.Add(m);
+        }
+    }
+}
---
+++ MediatorPatternDemo/GuiMediatorDemo/MarkerMediator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace GuiMediatorDemo
+{
+    public class MarkerMediator
+    {
+        //Collection of colleagues
+        private List<Marker> markers = new List<Marker>();
+
+        /// <summary>
+        /// CreateMarker creates a new colleague and adds it to the list of colleagues, and also sets the mediator on the new colleague to itself.
+        /// </summary>
+        /// <returns>Newly minted Marker instance</returns>
+        public Marker CreateMarker()
+        {
+            var m = new Marker();
+            m.SetMediator(this);
+            this.markers.Add(m);
+            return m;
+        }
+
+        /// <summary>
+        /// Send is used to receive a colleague's location, and to broadcast it to other colleagues except the sender
+        /// </summary>
+        /// <param name="location"></param>
+        /// <param name="marker"></param>
+        public void Send(Point location, Marker marker)
+        {
+            this.markers.Where(m => m != marker).ToList().ForEach(m => m.ReceiveLocation(location));
+        }
+    }
+}
