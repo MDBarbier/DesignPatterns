be698169541dd71516e48733f54f3cb2f2af9105
matt barbier <mdbarbier@protonmail.com>
2021-02-20 13:49:55 +0000

    Finished flyweight demo

---
+++ FlyweightDemo/FlyweightDemo/Beer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlyweightDemo
+{
+    public class Beer : IDrinkFlyweight
+    {
+        private string _name;
+        private IEnumerable<string> _ingredients;
+        public string Name { get { return _name; } }
+
+        public Beer()
+        {
+            _name = nameof(Beer);
+            _ingredients = new List<string>() { "water", "hops", "yeast" };
+        }
+
+        public void Serve(string size)
+        {
+            Console.WriteLine($"- {size} {_name} with {string.Join(", ", _ingredients)}");
+        }
+    }
+}
